9f5475c77061b8427abc5c9c5345f23f82db64a3
Colin But <colin.but@hotmail.co.uk>
2025-09-26 10:08:10 +0100

    refactor: clean

--- horoscope_checker.py
+++ horoscope_checker.py
@@ -8,63 +8,64 @@ def version():
 
 @app.command()
 def check(day: str, month: str):
+    day = int(day)
     if month.lower() == 'jan':
-        if int(day) > 19:
+        if day > 19:
             print("Aquarius")
         else:
             print("Capricorn")
     elif month.lower() == 'feb':
-        if int(day) > 18:
+        if day > 18:
             print("Pisces")
         else:
             print("Aquarius")
     elif month.lower() == 'mar':
-        if int(day) > 21:
+        if day > 21:
             print("Aries")
         else:
             print("Pisces")
     elif month.lower() == 'apr':
-        if int(day) > 19:
+        if day > 19:
             print("Taurus")
         else:
             print("Aries")
     elif month.lower() == 'may':
-        if int(day) > 20:
+        if day > 20:
             print("Gemini")
         else:
             print("Taurus")
     elif month.lower() == 'jun':
-        if int(day) > 20:
+        if day > 20:
             print("Cancel")
         else:
             print("Taurus")
     elif month.lower() == 'jul':
-        if int(day) > 22:
+        if day > 22:
             print("Leo")
         else:
             print("Cancel")
     elif month.lower() == 'aug':
-        if int(day) > 22:
+        if day > 22:
             print("Virgo")
         else:
             print("Leo")
     elif month.lower() == 'sep':
-        if int(day) > 22:
+        if day > 22:
             print("Libra")
         else:
             print("Virgo")
     elif month.lower() == 'oct':
-        if int(day) > 22:
+        if day > 22:
             print("Scorpio")
         else:
             print("Virgo")
     elif month.lower() == 'nov':
-        if int(day) > 21:
+        if day > 21:
             print("Sagittarius")
         else:
             print("Scorpio")
     elif month.lower() == 'dec':
-        if int(day) > 21:
+        if day > 21:
             print("Capricorn")
         else:
             print("Sagittarius")
