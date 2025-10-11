3e4179785334483af10bc55e7b60b29705362ae6
Colin But <colin.but@hotmail.co.uk>
2025-10-11 09:06:32 +0100

    tests: fix broken tests

--- src/horoscope/horoscope_checker.py
+++ src/horoscope/horoscope_checker.py
@@ -43,60 +43,82 @@ def check(day: str, month: str):
         else:
             return "ERROR"
     elif month.lower() == FEB:
-        if day > 18:
+        if 19 <= day <= 29:
             return PISCES
-        else:
+        elif 1 <= day <= 18:
             return AQUARIUS
+        else:
+            return "ERROR"
     elif month.lower() == MAR:
-        if day > 20:
+        if 21 <= day <= 31:
+            return ARIES
+        elif 1 <= day <= 20:
+            return PISCES
+        else: 
+            return "ERROR"
+    elif month.lower() == APR:
+        if 20 <= day <= 30:
+            return TAURUS
+        elif 1 <= day <= 19:
             return ARIES
         else:
-            return PISCES
-    elif month.lower() == APR:
-        if day > 19:
+            return "ERROR"
+    elif month.lower() == MAY:
+        if 21 <= day <= 31:
+            return GEMINI
+        elif 1 <= day <= 20:
             return TAURUS
         else:
-            return ARIES
-    elif month.lower() == MAY:
-        if day > 20:
+            return "ERROR"
+    elif month.lower() == JUN:
+        if 21 <= day <= 30:
+            return CANCER
+        elif 1 <= day <= 20:
             return GEMINI
         else:
-            return TAURUS
-    elif month.lower() == JUN:
-        if day > 20:
+            return "ERROR"
+    elif month.lower() == JUL:
+        if 23 <= day <= 32:
+            return LEO
+        elif 1 <= day <= 22:
             return CANCER
         else:
-            return GEMINI
-    elif month.lower() == JUL:
-        if day > 22:
+            return "ERROR"
+    elif month.lower() == AUG:
+        if 23 <= day <= 31:
+            return VIRGO
+        elif 1 <= day <= 22:
             return LEO
         else:
-            return CANCER
-    elif month.lower() == AUG:
-        if day > 22:
+            return "ERROR"
+    elif month.lower() == SEP:
+        if 23 <= day <= 30:
+            return LIBRA
+        elif 1 <= day <= 22:
             return VIRGO
         else:
-            return LEO
-    elif month.lower() == SEP:
-        if day > 22:
+            return "ERROR"
+    elif month.lower() == OCT:
+        if 23 <= day <= 31:
+            return SCORPIO
+        elif 1 <= day <= 22:
             return LIBRA
         else:
-            return VIRGO
-    elif month.lower() == OCT:
-        if day > 22:
-            return SCORPIO
-        else:
-            return VIRGO
+            return "ERROR"
     elif month.lower() == NOV:
-        if day > 21:
+        if 22 <= day <= 30:
             return SAGITTARIUS
-        else:
+        elif 1 <= day <= 21:
             return SCORPIO
-    elif month.lower() == DEC:
-        if day > 21:
-            return CAPRICORN
         else:
+            return "ERROR"
+    elif month.lower() == DEC:
+        if 22 <= day <= 31:
+            return CAPRICORN
+        elif 1 <= day <= 21:
              return SAGITTARIUS
+        else:
+            return "ERROR"
     else:
         pass
 
