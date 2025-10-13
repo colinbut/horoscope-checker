7979affc8780f64818cb2d7624b8e61888e125cd
Colin But <colin.but@hotmail.co.uk>
2025-10-13 14:16:13 +0100

    refactor: refactor code

--- src/horoscope/horoscope_checker.py
+++ src/horoscope/horoscope_checker.py
@@ -1,4 +1,5 @@
 import typer
+from months import Months
 
 app = typer.Typer()
 
@@ -19,100 +20,88 @@ LIBRA = "Libra"
 SAGITTARIUS = "Sagittarius"
 CAPRICORN = "Capricorn"
 
-JAN = "jan"
-FEB = "feb"
-MAR = "mar"
-APR = "apr"
-MAY = "may"
-JUN = "jun"
-JUL = "jul"
-AUG = "aug"
-SEP = "sep"
-OCT = "oct"
-NOV = "nov"
-DEC = "dec" 
 
 @app.command()
 def check(day: str, month: str):
     day = int(day)
-    if month.lower() == JAN:
+    if month.lower() == Months.JANUARY:
         if 20 <= day <= 31:
             return AQUARIUS
         elif 1 <= day <= 19:
             return CAPRICORN
         else:
             return "ERROR"
-    elif month.lower() == FEB:
+    elif month.lower() == Months.FEBRUARY:
         if 19 <= day <= 29:
             return PISCES
         elif 1 <= day <= 18:
             return AQUARIUS
         else:
             return "ERROR"
-    elif month.lower() == MAR:
+    elif month.lower() == Months.MARCH:
         if 21 <= day <= 31:
             return ARIES
         elif 1 <= day <= 20:
             return PISCES
         else: 
             return "ERROR"
-    elif month.lower() == APR:
+    elif month.lower() == Months.APRIL:
         if 20 <= day <= 30:
             return TAURUS
         elif 1 <= day <= 19:
             return ARIES
         else:
             return "ERROR"
-    elif month.lower() == MAY:
+    elif month.lower() == Months.MAY:
         if 21 <= day <= 31:
             return GEMINI
         elif 1 <= day <= 20:
             return TAURUS
         else:
             return "ERROR"
-    elif month.lower() == JUN:
+    elif month.lower() == Months.JUNE:
         if 21 <= day <= 30:
             return CANCER
         elif 1 <= day <= 20:
             return GEMINI
         else:
             return "ERROR"
-    elif month.lower() == JUL:
+    elif month.lower() == Months.JULY:
         if 23 <= day <= 32:
             return LEO
         elif 1 <= day <= 22:
             return CANCER
         else:
             return "ERROR"
-    elif month.lower() == AUG:
+    elif month.lower() == Months.AUGUST:
         if 23 <= day <= 31:
             return VIRGO
         elif 1 <= day <= 22:
             return LEO
         else:
             return "ERROR"
-    elif month.lower() == SEP:
+    elif month.lower() == Months.SEPTEMBER:
         if 23 <= day <= 30:
             return LIBRA
         elif 1 <= day <= 22:
             return VIRGO
         else:
             return "ERROR"
-    elif month.lower() == OCT:
+    elif month.lower() == Months.OCTOBER:
         if 23 <= day <= 31:
             return SCORPIO
         elif 1 <= day <= 22:
             return LIBRA
         else:
             return "ERROR"
-    elif month.lower() == NOV:
+    elif month.lower() == Months.NOVEMBER:
         if 22 <= day <= 30:
             return SAGITTARIUS
         elif 1 <= day <= 21:
             return SCORPIO
         else:
             return "ERROR"
-    elif month.lower() == DEC:
+    elif month.lower() == Months.DECEMBER:
         if 22 <= day <= 31:
             return CAPRICORN
         elif 1 <= day <= 21:
---
+++ src/horoscope/months.py
@@ -0,0 +1,15 @@
+from enum import StrEnum
+
+class Months(StrEnum):
+    JANUARY = "jan"
+    FEBRUARY = "feb"
+    MARCH = "mar"
+    APRIL = "apr"
+    MAY = "may"
+    JUNE = "jun"
+    JULY = "jul"
+    AUGUST = "aug"
+    SEPTEMBER = "sep"
+    OCTOBER = "oct"
+    NOVEMBER = "nov"
+    DECEMBER = "dec"
